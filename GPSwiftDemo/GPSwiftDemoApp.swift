e55500f16c7b4113d8c1e001e1b055b2783370e7
Akshay A <auitech@Akshays-Macbook-Pro.local>
2024-01-01 00:43:52 +0530

    Initial commit: Set up iOS project structure. Created the initial project structure for our iPhone app. This commit includes the Xcode project setup, basic folder structure, and necessary configuration files and some code

---
+++ GPSwiftDemo/GPSwiftDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GPSwiftDemoApp.swift
+//  GPSwiftDemo
+//
+//  Created by AUI Tech on 28/12/23.
+//
+
+import SwiftUI
+
+@main
+struct GPSwiftDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            SummaryListView()
+        }
+    }
+}
